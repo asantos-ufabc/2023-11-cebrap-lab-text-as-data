1915a677cbaf9dfc4dd7df34a437e79d31dea322
Beatriz Milz <milz.bea@gmail.com>
2023-11-27 09:12:33 -0300

    documentando

--- scripts/01-download-dados.R
+++ scripts/01-download-dados.R
@@ -20,17 +20,21 @@ library(janitor)
 # Proposto por
 # Luiz Philippe de Orleans e Bragança (PL-SP) 
 
+# ID da enquete é o numero que aparece no final da URL
 id_enquete <- "2373385"
 
+# URL para baixar os dados
 url <- paste0(
   "https://www.camara.leg.br/enquetes/posicionamentos/download/todos-posicionamentos?idEnquete=",
   id_enquete,
   "&exibicao=undefined&ordenacao=undefined"
 )
 
+# Baixando os dados, limpando o nome das colunas,
+# salvando o id da enquete como uma coluna
 resultados_enquete <- read_csv(url, skip = 1) |>
   clean_names() |>
   mutate(id = id_enquete, .before = everything())
 
-
+# Salvando os dados
 write_rds(resultados_enquete, "dados-brutos/resultados_enquete.rds")
